d9339e13b03bce05658ce6922d99b296d593a867
Rodrigo <rodrigo.freire@msn.com>
2024-04-16 08:44:39 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _ProjetoMVC_FInal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
